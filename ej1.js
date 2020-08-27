9e44ea2ad73b8f5cb3d2309d64a6b5ae0aba73d7
Croperinho <63494559+Croperinho@users.noreply.github.com>
2020-08-27 09:50:17 -0300

    ej 1 y 2

---
+++ ej1.js
@@ -0,0 +1,7 @@
+/* 
+Ejercicio 1
+Imprimir por consola un mensaje guardado en una variable usando la función console.log()
+*/
+
+var helloworld = "Hello world.";
+console.log(helloworld);
---
+++ ej2.js
@@ -0,0 +1,12 @@
+/* 
+Ejercicio 2
+Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla los doce nombres utilizando la función console.log()
+*/
+
+var mes = (['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']);
+
+var i;
+
+for (i=0;i < mes.length; i++){
+    console.log(mes[i]);
+}
