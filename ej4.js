43c1c733a62f57928b89622c2e7f427053dd83d9
Croperinho <63494559+Croperinho@users.noreply.github.com>
2020-08-31 18:38:47 -0300

    Update ej4.js

--- ej4.js
+++ ej4.js
@@ -3,3 +3,10 @@ Ejercicio 4
 Define una función que dado un array de números nos dice si alguno es par.
 */
 
+var nums = ([1,2,3,4,5,6]);
+
+function VerificarPar(num){
+    return num%2 === 0;
+}
+
+console.log( nums.some(VerificarPar) );
